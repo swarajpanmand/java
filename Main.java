b1172d6d40d881901339fe212a841d5928c37d87
Swaraj Panmand <swarajpanmand@gmail.com>
2024-01-25 21:55:45 +0530

    second commit

---
+++ Main.java
@@ -0,0 +1,77 @@
+package java;
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void printJava(){
+        System.out.println("print hello");
+    }
+
+    public static void main(String[] args) {
+
+        printJava();
+        //        int age= 30;
+//        int[] marks= new int[4];
+//        marks[0]= 32;
+//        marks[1]= 45;
+//        marks[2]= 67;
+//        System.out.println(marks[3]);
+//        System.out.println(marks.length);
+//
+//        Arrays.sort(marks);
+//        System.out.println(marks[1]);
+//
+//        int[][] final-marks ={{56,85,23},{7858,78,669}};
+//        System.out.println(final-marks[0][0]);
+//
+//        int p=100;
+//        int fp= p + (int)18.230;
+//        System.out.println(fp);
+//
+//        int  aa=2;
+//        int b=4;
+//        int div=aa%b;
+//        System.out.println(aa-5);
+//        System.out.println((int)(Math.random()*100));
+//
+//        System.out.println("input your age: ");
+//        Scanner sc= new Scanner(System.in);
+//        String name = sc.next();
+//        System.out.println(name);
+//
+//        boolean is-sunup = true;
+//        if(is-sunup == true)
+//            System.out.println("day");
+//        else
+//            System.out.println("night");
+
+//        int day=2;
+//        switch(day)
+//        {
+//            case 1:
+//                System.out.println("monday");
+//                break;
+//            case 2:
+//                System.out.println("tuesday");
+//                break;
+//            case 3:
+//                System.out.println("monday");
+//        }
+
+//        for(int i =100; i>=1; i--)
+//            System.out.println(i);
+
+//        Scanner sc = new Scanner(System.in);
+//        int number;
+//        do{
+//            System.out.println("input a number");
+//            number = sc.nextInt();
+//            System.out.print("here is your number: ");
+//            System.out.println(number);
+//
+//        }while(number>=0);
+    }
+}
